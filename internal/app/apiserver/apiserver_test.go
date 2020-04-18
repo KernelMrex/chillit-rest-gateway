c76c37e82bf0d6c7787313b8832c45d00710a7be
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-04-18 12:09:31 +0300

    Implemented 'GET /places?amount=?&offset=?'

--- internal/app/apiserver/apiserver_test.go
+++ internal/app/apiserver/apiserver_test.go
@@ -3,6 +3,7 @@ package apiserver
 import (
 	"bytes"
 	"chillit-rest-gateway/internal/app/places"
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -15,7 +16,7 @@ const (
 	grpcServerPort = "10324"
 )
 
-func TestServer_postPlaceHandler_AllClear(t *testing.T) {
+func TestServer_postPlaceHandler_Success(t *testing.T) {
 	// Building places client
 	mockStoreClient := &places.MockPlacesStoreClient{
 		AddPlaceResponse: &places.AddPlaceResponse{
@@ -79,4 +80,64 @@ func TestServer_postPlaceHandler_UnavailablePlacesStore(t *testing.T) {
 	server.ServeHTTP(rec, req)
 
 	assert.Equal(t, http.StatusBadGateway, rec.Code)
+}
+
+func TestServer_getPlacesHandler_Success(t *testing.T) {
+	// Building places client
+	mockStoreClient := &places.MockPlacesStoreClient{
+		GetPlacesResponse: &places.GetPlacesResponse{
+			Places: []*places.Place{
+				{
+					Id:          1,
+					Title:       "test title 1",
+					Address:     "test address 1",
+					Description: "test description 1",
+				},
+				{
+					Id:          2,
+					Title:       "test title 2",
+					Address:     "test address 2",
+					Description: "test description 2",
+				},
+			},
+		},
+	}
+
+	// Building api server
+	server := newServer(mockStoreClient)
+
+	// Executing request on test server
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/places?offset=0&amount=20", nil)
+	server.ServeHTTP(rec, req)
+
+	// Checking response
+	type responsePlace struct {
+		ID          uint64 `json:"id"`
+		Title       string `json:"title"`
+		Address     string `json:"address"`
+		Description string `json:"description"`
+	}
+	type response struct {
+		Places []*responsePlace `json:"places"`
+	}
+	respBody, _ := json.Marshal(response{
+		Places: []*responsePlace{
+			{
+				ID:          1,
+				Title:       "test title 1",
+				Address:     "test address 1",
+				Description: "test description 1",
+			},
+			{
+				ID:          2,
+				Title:       "test title 2",
+				Address:     "test address 2",
+				Description: "test description 2",
+			},
+		},
+	})
+
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, string(rec.Body.Bytes()), string(respBody)+"\n")
 }
--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -4,6 +4,7 @@ import (
 	"chillit-rest-gateway/internal/app/places"
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
@@ -28,7 +29,7 @@ func newServer(placesStore places.PlacesStoreClient) *server {
 // TODO: add logging middleware
 func (s *server) configureRouter() {
 	s.router.HandleFunc("/place", s.postPlaceHandler()).Methods(http.MethodPost)
-	s.router.HandleFunc("/places", s.notImplementedHandler()).Methods(http.MethodGet)
+	s.router.HandleFunc("/places", s.getPlacesHandler()).Methods(http.MethodGet)
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -60,6 +61,71 @@ func (s *server) postPlaceHandler() http.HandlerFunc {
 			s.logger.Errorln("[ PostPlaceHandler ] error while sending request to store service:", err)
 			w.WriteHeader(http.StatusBadGateway)
 			return
+		}
+	})
+}
+
+func (s *server) getPlacesHandler() http.HandlerFunc {
+	const methodName string = "GET /places"
+
+	// Got as URLQuery
+	type request struct {
+		Offset uint64
+		Amount uint64
+	}
+	type responsePlace struct {
+		ID          uint64 `json:"id"`
+		Title       string `json:"title"`
+		Address     string `json:"address"`
+		Description string `json:"description"`
+	}
+
+	type response struct {
+		Places []*responsePlace `json:"places"`
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var reqPlaces request
+		urlVals := r.URL.Query()
+
+		var err error
+		reqPlaces.Offset, err = strconv.ParseUint(urlVals.Get("offset"), 10, 64)
+		if err != nil {
+			s.logger.Errorf("bad parameters for '%s' request: amount='%v'", methodName, r.URL.RawQuery)
+			w.WriteHeader(http.StatusRequestedRangeNotSatisfiable)
+			return
+		}
+
+		reqPlaces.Amount, err = strconv.ParseUint(urlVals.Get("amount"), 10, 64)
+		if err != nil || reqPlaces.Amount == 0 {
+			s.logger.Errorf("bad parameters for '%s' request: amount='%v'", methodName, r.URL.RawQuery)
+			w.WriteHeader(http.StatusRequestedRangeNotSatisfiable)
+			return
+		}
+
+		placesStoreResp, err := s.placesStore.GetPlaces(r.Context(), &places.GetPlacesRequest{
+			Amount: reqPlaces.Amount,
+			Offset: reqPlaces.Offset,
+		})
+		if err != nil {
+			s.logger.Errorf("error in '%s' while requesting places from placesStore: %v", methodName, err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+
+		respPlaces := make([]*responsePlace, len(placesStoreResp.Places))
+		for i, place := range placesStoreResp.Places {
+			respPlaces[i] = &responsePlace{
+				ID:          place.Id,
+				Title:       place.Title,
+				Address:     place.Address,
+				Description: place.Description,
+			}
+		}
+
+		if err := json.NewEncoder(w).Encode(response{Places: respPlaces}); err != nil {
+			s.logger.Errorf("error in '%s' while encoding json response", methodName)
+			w.WriteHeader(http.StatusInternalServerError)
 		}
 	})
 }
